99afaf8e0d93cfc21ec7cf63938800a35519ef90
petar111 <jpetar765@gmail.com>
2023-10-19 11:28:04 +0200

    Initialized heroes authorization server project.

---
+++ src/main/java/com/springpj/heroesauthorizationserver/HeroesauthorizationserverApplication.java
@@ -0,0 +1,13 @@
+package com.springpj.heroesauthorizationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroesauthorizationserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeroesauthorizationserverApplication.class, args);
+	}
+
+}
